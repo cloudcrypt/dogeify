2f3fd2fe6bdd206f386f38250eafe6e60d26ee0f
Daniel D <cloudcrypt@hotmail.com>
2015-04-25 17:36:53 -0600

    fix

--- dogeify.py
+++ dogeify.py
@@ -3,8 +3,6 @@ import random
 import sys
 from tqdm import *
 
-adjs = ['so', 'such', 'very', 'much', 'many', 'how']
-emots = ['wow', 'amaze', 'excite']
 
 sentence = "The brown fox jumped over the lazy dog. The test, the sentence."
 
@@ -29,49 +27,22 @@ def dogeify(text):
 					resultArray.append("so")
 				#resultArray.append("such")
 		else:
-			if nltk.pos_tag(nltk.word_tokenize(word))[0][1][:1] == "N":
-				print word
 			if nltk.pos_tag(nltk.word_tokenize(word))[0][1][:1] == "V":
 				randNum = random.randint(1,2)
 				if randNum == 1:
-					#resultArray.append("much")
-					resultArray.append(random.choice(adjs))
+					resultArray.append("much")
 					resultArray.append(word)
 				elif randNum == 2:
-					#resultArray.append("many")
-					resultArray.append(random.choice(adjs))
+					resultArray.append("many")
 					resultArray.append(word)
 			else:
 				resultArray.append(word)
 		index += 1
 	return " ".join(resultArray)
 	
-def superdogeify(text):
-	print " "
-	textArray = text.split()
-	nouns = []
-	resultArray = []
-	for word in tqdm(textArray):
-		if nltk.pos_tag(nltk.word_tokenize(word))[0][1][:1] == "N":
-			nouns.append(word)
-	for word in nouns:
-		randNum = random.randint(1,100)
-		if randNum <= 10:
-			resultArray.append("So wow.")
-		elif randNum <= 20:
-			resultArray.append("Wow.")
-		resultArray.append(random.choice(adjs).capitalize())
-		resultArray.append((word + "."))
-
-	return " ".join(resultArray)
-	
-	
-	
-	
-	
 def main():
 	userString = raw_input("Enter text:")
-	result = superdogeify(userString)
+	result = dogeify(userString)
 	print ""
 	print result
 
