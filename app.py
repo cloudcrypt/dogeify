2a56f2c8d0376c9273669cc5c4686981b289e274
Daniel D <cloudcrypt@hotmail.com>
2015-07-16 23:44:15 -0400

    Duplicate adjective fix

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, url_for, make_response, session, redirect, g, flash
+import random, urllib
 from dogeify import *
 from colors import *
-import random
 
 
 app = Flask(__name__)
 # app.config.from_object(__name__)
-#app.secret_key = '\xdc\xae\xd5\xce\x9a\x83\x8d\xe1\x0e\xe7K>\xc5O\x18\xa0\r6\x87=\xf8\xe3<\x88'
+app.secret_key = '\xa6e\x93\xaf\xb4\xfax\xcc4\x8cB\xb6m\xce\x80\xdcu~\xa7|\xa6\xadQ\xd9'
 
 
 
@@ -23,8 +23,24 @@ def dogeifyText():
     for dogePair in dogeTextArray:
         dogeResult.append([dogePair, random.choice(htmlColors.keys())])
     result = [text, dogeResult]
+    #processHistory(text)
     return render_template("dogetext.html", result=result)
+    
+@app.route('/clearhistory')
+def clearHistory():
+    session.pop('history')
+    flash("History has been cleared.")
+    return render_template("home.html", flashType="success")    
 
+def processHistory(set):
+    historyPair = [set, urllib.quote_plus(set)]
+    if 'history' not in session:
+        session['history'] = []
+    if historyPair in session['history']:
+        return
+    else:
+        session['history'].append(historyPair)
+        return
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
