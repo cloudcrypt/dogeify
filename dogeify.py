66894638a55b585c7e49892af0ec32d69fd1cc19
Hugh Young <hy@thisishughyoung.thisishughyoung>
2015-07-17 00:26:53 -0400

    modified 'so wow' and 'wow' mechanics. Also compare with previous two adjectives, rather than just one

--- dogeify.py
+++ dogeify.py
@@ -54,15 +54,18 @@ def superdogeify(text):
 		if nltk.pos_tag(nltk.word_tokenize(word))[0][1][:1] == "N":
 			nouns.append(word)
 	lastAdj = ""
+	lastAdj2 = ""
 	for word in nouns:
 		randNum = random.randint(1,100)
-		if randNum <= 10:
+		if randNum <= 5 and lastAdj != "so" and lastAdj2 != "so":
 			resultArray.append("So wow.")
-		elif randNum <= 20:
+		elif randNum <= 10:
 			resultArray.append("Wow.")
 		tempAdjs = list(adjs)
 		if lastAdj in adjs: tempAdjs.remove(lastAdj)
+		if lastAdj2 in adjs: tempAdjs.remove(lastAdj2)
 		randomAdj = random.choice(tempAdjs)
+		lastAdj2 = lastAdj
 		lastAdj = randomAdj
 		resultArray.append(randomAdj.capitalize() + ( " " + word.rstrip('?:!.,;') + "."))
 		#resultArray.append((word.rstrip('?:!.,;') + "."))
