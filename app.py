c200d4699574cd841591c1c2f96ce341f975cc89
ThisIsHughYoung <ThisIsHughYoung@gmail.com>
2015-07-19 22:47:58 -0400

    very optimization. so feature. much cut.

--- app.py
+++ app.py
@@ -6,25 +6,9 @@ import dogeconfig
 
 app = Flask(__name__)
 # app.config.from_object(__name__)
-app.secret_key = dogeconfig.secret_key
-
-
-
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    # if request.method == "GET":
-    #     text = request.args.get("userText")
-    #     if len(text) > 2000:
-    #         flash("Text has exceeded length limit!")
-    #         return render_template("home.html", flashType="danger")  
-    #     dogeTextArray = superdogeify(text)
-    #     dogeResult = []
-    #     for dogePair in dogeTextArray:
-    #         dogeResult.append([dogePair, random.choice(htmlColors.keys())])
-    #     result = [text, dogeResult]
-    #     processHistory(text)
-    #     return jsonify(list=result)
     getMode = False
     return render_template("home.html", getMode=getMode, result=[])    
 
@@ -43,30 +27,11 @@ def dogeifyText():
     for dogePair in dogeTextArray:
         dogeResult.append([dogePair, random.choice(htmlColors.keys())])
     result = [text, dogeResult]
-    processHistory(text)
     if request.method == "GET":
         getMode = True
         return render_template("home.html", getMode=getMode, result=result)
     elif request.method == "POST":
         return jsonify(list=result)
             
-@app.route('/clearhistory')
-def clearHistory():
-    session.pop('history')
-    flash("History has been cleared.")
-    return render_template("home.html", flashType="success")    
-
-def processHistory(set):
-    if len(set) > 150:
-        return
-    historyPair = [set, urllib.quote_plus(set.encode('ascii', 'ignore'))]
-    if 'history' not in session:
-        session['history'] = []
-    if historyPair in session['history']:
-        return
-    else:
-        session['history'].append(historyPair)
-        return
-
 if __name__ == '__main__':
-    app.run(host=dogeconfig.host, port=dogeconfig.port, debug=True)
+    app.run(host=dogeconfig.host, port=dogeconfig.port)
