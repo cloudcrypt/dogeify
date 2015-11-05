fe100914daef1f3cfe3af151723656e691d3de1d
Hugh Young <hy.tritone@gmail.com>
2015-11-05 17:48:44 -0500

    such literature

--- quotes.py
+++ quotes.py
@@ -1,1 +1,136 @@
-quotes = ['Confucius say, man who run in front of car get tired. man who run behind car get exhausted.', 'Confucius say, war not determine who is right, war determine who is left.', 'Confucius say, man who drive like hell, bound to get there.', 'Confucius say, man who sit on tack get point!', 'Confucius say, man who jump off cliff, jump to conclusion!', 'Confucius say, when called an idiot, better to be quiet than open mouth and remove all doubt.', 'Confucius say, man who walk through airport turnstile sideways going to bangkok.', 'Confucius say, he who thinks only of number one must remember this number is next to nothing.', 'Confucius say, man who put head on railroad track to listen for train likely to end up with splitting headache.', 'Confucius say, man who eats photo of father, soon spitting-image of father.', 'Confucius say, man who pushes piano down mineshaft get tone of a flat miner.', 'Confucius say, man who fall in vat of molten glass make spectacle of self.', 'Confucius say, man who sneezes without tissue takes matters in own hands.', 'Confucius say, man who eat many prunes get good run for money.', 'Confucius say, woman who wear jockstrap have make believe ballroom.', 'Confucius say, marriage is like game of poker. you start with pair and end with full house.', 'Confucius say, it take many nails to build crib, but one screw to fill it.', 'Confucius say, man who stand on toilet high on pot.', 'Confucius say, man who lay girl on hillside is not on the level.', 'Confucius say, woman laid in tomb may become mummy.', 'Confucius say, man who cut self while shaving, lose face.']
+quotes = [
+#Romeo and Juliet - Shakespeare
+"""Two households, both alike in dignity,
+In fair Verona, where we lay our scene,
+From ancient grudge break to new mutiny,
+Where civil blood makes civil hands unclean.
+From forth the fatal loins of these two foes
+A pair of star-cross'd lovers take their life;
+Whose misadventured piteous overthrows
+Do with their death bury their parents' strife.
+The fearful passage of their death-mark'd love,
+And the continuance of their parents' rage,
+Which, but their children's end, nought could remove,
+Is now the two hours' traffic of our stage;
+The which if you with patient ears attend,
+What here shall miss, our toil shall strive to mend.""",
+
+#Hamlet - Shakespeare
+"""To be, or not to be - that is the question:
+Whether 'tis nobler in the mind to suffer
+The slings and arrows of outrageous fortune
+Or to take arms against a sea of troubles
+And by opposing end them. To die, to sleep - 
+No more - and by a sleep to say we end
+The heartache, and the thousand natural shocks
+That flesh is heir to. 'Tis a consummation
+Devoutly to be wished. To die, to sleep - 
+To sleep - perchance to dream: ay, there's the rub,
+For in that sleep of death what dreams may come
+When we have shuffled off this mortal coil,
+Must give us pause. There's the respect
+That makes calamity of so long life.
+For who would bear the whips and scorns of time,
+Th' oppressor's wrong, the proud man's contumely
+The pangs of despised love, the law's delay,
+The insolence of office, and the spurns
+That patient merit of th' unworthy takes,
+When he himself might his quietus make
+With a bare bodkin? Who would fardels bear,
+To grunt and sweat under a weary life,
+But that the dread of something after death,
+The undiscovered country, from whose bourn
+No traveller returns, puzzles the will,
+And makes us rather bear those ills we have
+Than fly to others that we know not of?
+Thus conscience does make cowards of us all,
+And thus the native hue of resolution
+Is sicklied o'er with the pale cast of thought,
+And enterprise of great pitch and moment
+With this regard their currents turn awry
+And lose the name of action.  -  Soft you now,
+The fair Ophelia!  -  Nymph, in thy orisons
+Be all my sins remembered.""",
+
+#Tale of Two Cities - C. Dickens
+"""It was the best of times,
+it was the worst of times,
+it was the age of wisdom,
+it was the age of foolishness,
+it was the epoch of belief,
+it was the epoch of incredulity,
+it was the season of Light,
+it was the season of Darkness,
+it was the spring of hope,
+it was the winter of despair,
+we had everything before us,
+we had nothing before us,
+we were all going direct to Heaven,
+we were all going direct the other way - 
+in short, the period was so far like the present period, that some of
+its noisiest authorities insisted on its being received, for good or for
+evil, in the superlative degree of comparison only.""",
+
+#Metamorphosis - Franz Kafka
+"""One morning, when Gregor Samsa woke from troubled dreams, 
+he found himself transformed in his bed into a horrible vermin. 
+He lay on his armour-like back, and if he lifted his head a 
+little he could see his brown belly, slightly domed and divided 
+by arches into stiff sections. The bedding was hardly able to 
+cover it and seemed ready to slide off any moment. His many legs, 
+pitifully thin compared with the size of the rest of him, waved 
+about helplessly as he looked.""",
+
+#Communist Manifesto
+"""The history of all hitherto existing society is the history 
+of class struggles.
+
+Freeman and slave, patrician and plebeian, lord and serf, 
+guild-master and journeyman, in a word, oppressor and oppressed, 
+stood in constant opposition to one another, carried on an 
+uninterrupted, now hidden, now open fight, a fight that each 
+time ended, either in a revolutionary reconstitution of society 
+at large, or in the common ruin of the contending classes.
+
+In the earlier epochs of history, we find almost everywhere a 
+complicated arrangement of society into various orders, a manifold 
+gradation of social rank. In ancient Rome we have patricians, 
+knights, plebeians, slaves; in the Middle Ages, feudal lords, 
+vassals, guild-masters, journeymen, apprentices, serfs; in almost 
+all of these classes, again, subordinate gradations.
+
+The modern bourgeois society that has sprouted from the ruins of 
+feudal society has not done away with class antagonisms. It has 
+but established new classes, new conditions of oppression, new 
+forms of struggle in place of the old ones.""",
+
+#US Declaration of Independence
+"""We hold these truths to be self-evident, that all men are created 
+equal, that they are endowed by their Creator with certain unalienable 
+Rights, that among these are Life, Liberty and the pursuit of Happiness.
+That to secure these rights, Governments are instituted among Men, 
+deriving their just powers from the consent of the governed, 
+That whenever any Form of Government becomes destructive of these ends, 
+it is the Right of the People to alter or to abolish it, and to institute 
+new Government, laying its foundation on such principles and organizing 
+its powers in such form, as to them shall seem most likely to effect 
+their Safety and Happiness. Prudence, indeed, will dictate that 
+Governments long established should not be changed for light and 
+transient causes; and accordingly all experience hath shewn, that mankind 
+are more disposed to suffer, while evils are sufferable, than to right 
+themselves by abolishing the forms to which they are accustomed. But when a 
+long train of abuses and usurpations, pursuing invariably the same Object 
+evinces a design to reduce them under absolute Despotism, it is their right, 
+it is their duty, to throw off such Government, and to provide new Guards 
+for their future security.""",
+
+#Lolita - Vivian Darkbloom
+"""Lolita, light of my life, fire of my loins. My sin, my soul. Lo-lee-ta:
+the tip of the tongue taking a trip of three steps down the palate to tap,
+at three, on the teeth. Lo. Lee. Ta.
+
+She was Lo, plain Lo, in the morning, standing four feet ten in one
+sock. She was Lola in slacks. She was Dolly at school. She was Dolores on
+the dotted line. But in my arms she was always Lolita"""
+
+]
