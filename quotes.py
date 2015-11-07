341769eada3bca1ff534b6bfa70dbbc05dc15894
Hugh Young <hy.tritone@gmail.com>
2015-11-07 18:31:05 -0500

    many reduction

--- quotes.py
+++ quotes.py
@@ -75,7 +75,7 @@ But bears it out even to the edge of doom.
   I never writ, nor no man ever loved.""",
 
 #===============================================================
-# Literature & Poetry
+# Literature
 
 #Tale of Two Cities - C. Dickens
 """It was the best of times,
@@ -129,6 +129,9 @@ He had so heated himself with rapid walking in the fog and frost, this nephew of
 "I do," said Scrooge. "Merry Christmas! What right have you to be merry? What reason have you to be merry? You're poor enough."
 "Come, then," returned the nephew gaily. "What right have you to be dismal? What reason have you to be morose? You're rich enough."
 Scrooge having no better answer ready on the spur of the moment, said, "Bah!" again; and followed it up with "Humbug.\"""",
+
+#===============================================================
+# Poetry
 
 #The Cremation of Sam McGee - Robert W. Service
 """There are strange things done in the midnight sun
@@ -217,28 +220,7 @@ their Safety and Happiness.""",
 #Gettysburg Address - A. Lincoln
 """Four score and seven years ago our fathers brought forth on this 
 continent, a new nation, conceived in Liberty, and dedicated to the 
-proposition that all men are created equal.
-
-Now we are engaged in a great civil war, testing whether that nation, 
-or any nation so conceived and so dedicated, can long endure. We are 
-met on a great battle-field of that war. We have come to dedicate a portion 
-of that field, as a final resting place for those who here gave their lives 
-that that nation might live. It is altogether fitting and proper that we 
-should do this.
-
-But, in a larger sense, we can not dedicate -- we can not consecrate -- 
-we can not hallow -- this ground. The brave men, living and dead, who 
-struggled here, have consecrated it, far above our poor power to add or 
-detract. The world will little note, nor long remember what we say here, 
-but it can never forget what they did here. It is for us the living, rather, 
-to be dedicated here to the unfinished work which they who fought here have 
-thus far so nobly advanced. It is rather for us to be here dedicated to 
-the great task remaining before us -- that from these honored dead we take 
-increased devotion to that cause for which they gave the last full measure 
-of devotion -- that we here highly resolve that these dead shall not have 
-died in vain -- that this nation, under God, shall have a new birth of 
-freedom -- and that government of the people, by the people, for the 
-people, shall not perish from the earth.""",
+proposition that all men are created equal.""",
 
 #===============================================================
 # Philosophy
